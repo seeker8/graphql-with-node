c1ac59214bd3955a5b24e95f1c79a32e39102ac7
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-23 16:10:43 -0400

    add schema

--- crudgraphql/server.js
+++ crudgraphql/server.js
@@ -1,6 +1,20 @@
 const express = require("express")
+const { buildSchema } = require("graphql")
+const courses = require("./courses")
 
 const app = express()
+
+const schema = buildSchema(`
+type Course {
+    id: ID!
+    title: String!
+    views: Int
+}
+
+type Query {
+    getCourses: [Course]
+}
+`)
 
 app.get("/", (req, resp) => {
   resp.send("Welcome")
