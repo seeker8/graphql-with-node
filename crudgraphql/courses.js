e975f24562de5a4abbcda90773f6a203a886a414
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-23 16:10:29 -0400

    add mocked data

---
+++ crudgraphql/courses.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: "1", title: "GraphQL and Node", views: 1000 },
+  { id: "2", title: "JavaScript", views: 1500 },
+  { id: "3", title: "Node JS from zero to hero", views: 1700 }
+]
