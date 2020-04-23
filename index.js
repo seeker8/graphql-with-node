f776246e689df511dc2d08299a898785b2433c61
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-23 14:30:51 -0400

    add a basic graphql schema

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
+const express = require("express")
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  graphql,
+} = require("graphql")
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hola Mundo")
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      message: {
+        type: GraphQLString,
+        resolve() {
+          return "Hola Mundo"
+        },
+      },
+    },
+  }),
+})
+
+app.get("/", (req, res) => {
+  graphql(
+    schema,
+    `
+      {
+        message
+      }
+    `
+  ).then((r) => res.send(r))
 })
 
 app.listen(3000, () => {
-    console.log('server listening on port 3000')
+  console.log("server listening on port 3000")
 })
