69a92bbcd57bb38122e01856ecdb1b6e68d1a567
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-24 14:08:48 -0400

    add pagination

--- crudgraphql/courses.js
+++ crudgraphql/courses.js
@@ -1,5 +1,11 @@
 module.exports = [
   { id: "1", title: "GraphQL and Node", views: 1000 },
   { id: "2", title: "JavaScript", views: 1500 },
-  { id: "3", title: "Node JS from zero to hero", views: 1700 }
+  { id: "3", title: "Go Lang", views: 1500 },
+  { id: "4", title: "Ruby on Rails", views: 1500 },
+  { id: "5", title: "SpringBoot", views: 1500 },
+  { id: "6", title: "Mastering Vue", views: 1500 },
+  { id: "7", title: "ReactJs", views: 1500 },
+  { id: "8", title: "Intro to TypeScript", views: 1500 },
+  { id: "9", title: "Node JS from zero to hero", views: 1700 }
 ]
