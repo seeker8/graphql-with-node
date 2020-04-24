458557f72634d1dace6aa78e28d4ae64f28d83c3
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-24 17:37:34 -0400

    define initial schema and type definitions

--- apollo/server.js
+++ apollo/server.js
@@ -1,7 +1,26 @@
 const { ApolloServer } = require("apollo-server")
+const { makeExecutableSchema } = require("graphql-tools")
+
+const typeDefs = `
+type Course {
+    id: ID!
+    title: String!
+    views: Int
+}
+
+type Query {
+    getCourses(page: Int, limit: Int = 1): [Course]
+    getCourse(id: ID!): Course
+}
+`
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers: {}
+})
 
 const server = new ApolloServer({
-  schema: null
+  schema
 })
 
 server.listen().then(({ url }) => {
