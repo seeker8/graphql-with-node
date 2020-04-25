4b13d90545d0c0067972926bad82764ab079a253
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-25 13:20:43 -0400

    refactor resolvers and add mutation addCourse

--- apollo/server.js
+++ apollo/server.js
@@ -9,24 +9,44 @@ type Course {
     views: Int
 }
 
+input CourseInput {
+    title: String!
+    views: Int
+}
+
 type Query {
     getCourses(page: Int, limit: Int = 1): [Course]
     getCourse(id: ID!): Course
 }
+
+type Mutation {
+    addCourse(input: CourseInput): Course
+}
 `
+
+const resolvers = {
+  Query: {
+    getCourses(obj, { page, limit }) {
+      if (limit >= courses.length || page == undefined) return courses
+      let start = page * limit - limit
+      let end = start + limit
+      return courses.slice(start, end)
+    }
+  },
+  Mutation: {
+    addCourse(obj, { input }) {
+      const { title, views = 0 } = input
+      const id = String(courses.length + 1)
+      const newCourse = { id, title, views }
+      courses.push(newCourse)
+      return newCourse
+    }
+  }
+}
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers: {
-    Query: {
-      getCourses(obj, { page, limit }) {
-        if (limit >= courses.length || page == undefined) return courses
-        let start = page * limit - limit
-        let end = start + limit
-        return courses.slice(start, end)
-      }
-    }
-  }
+  resolvers
 })
 
 const server = new ApolloServer({
