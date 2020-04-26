a10f6ae52863d929e0801d1b6bb5085b288610bc
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-26 12:12:35 -0400

    set default value for views

--- dbex/models/course.js
+++ dbex/models/course.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const courseSchema = new mongoose.Schema({
   title: String,
-  views: Number
+  views: { type: Number, default: 0 }
 })
 
 module.exports = mongoose.model("Course", courseSchema)
