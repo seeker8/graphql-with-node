60372066c792f6ed14590563b38d8d69f18f74b5
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-04-26 13:10:29 -0400

    update pagination

--- dbex/resolvers/course.resolvers.js
+++ dbex/resolvers/course.resolvers.js
@@ -1,11 +1,13 @@
 const Course = require("../models/course")
 
-let courses = []
 module.exports = {
   Query: {
     async getCourses(obj, { page, limit }) {
-      const courses = await Course.find()
-      return courses
+      let courses = Course.find()
+      if (page) {
+        courses = courses.limit(limit).skip(page * limit - limit)
+      }
+      return await courses
     },
     async getCourse(obj, { id }) {
       return await Course.findById(id)
